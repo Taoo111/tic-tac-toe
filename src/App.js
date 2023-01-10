fa20437587c021856b1d6ca0a883b6fc6ec0dbec
Dawid <dawidjura12@gmail.com>
2023-01-10 15:02:06 +0100

    Added functionality of updating board on click event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Board from "./components/Board"
+import { useState } from "react";
+
+import Board from "./components/Board";
 import "./App.css";
 
 function App() {
-  const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const handleSquareClick = (squareID) => {
+    const updatedBoard = board.map((value, index) => {
+      if (index === squareID) {
+        return "X";
+      } else return value;
+
+    });
+    setBoard(updatedBoard)
+  };
   return (
     <div className="App">
       <p>hello worlds</p>
-      <Board board={board} onClick={null}/>
-
+      <Board board={board} onClick={handleSquareClick} />
     </div>
   );
 }
